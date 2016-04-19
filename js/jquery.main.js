e77368e42dc4b081d9456a808fce9b281c7aad8e
Sergey <kuzmenkos1986@gmail.com>
2016-04-19 11:44:20 +0300

    bugs

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -12,88 +12,97 @@ var Menu = function( obj ) {
     var _obj = $( this ),
         _objBtn = $( '.header-menu__btn'),
         _menu = $( '.header-menu' ),
-        _items = $( '.header-menu__wrap>li' ),
+        _items = $( '.header-menu__has-sub' ),
         _window = $( window );
 
     //private methods
     var _addEvents = function() {
 
-                _objBtn.on( {
-                    click: function() {
-                        var curElem = _objBtn.parent();
-                        if (curElem.hasClass( 'active' )) {
-                            curElem.removeClass( 'active' );
-                        } else {
-                            curElem.addClass( 'active' );
+            _objBtn.on( {
+                click: function() {
+                    var curElem = _objBtn.parent();
+                    if (curElem.hasClass( 'active' )) {
+                        curElem.removeClass( 'active' );
+                    } else {
+                        curElem.addClass( 'active' );
+                    }
+                    if (event.stopPropagation) {
+                        event.stopPropagation();
+                    } else {
+                        event.cancelBubble = true;
+                    }
+                }
+            });
+
+            _items.on( {
+                click: function() {
+
+                    if ( $( window ).width() < 768 ) {
+
+                        var curElem = $( this ),
+                            curMenu = curElem.find( 'ul' );
+
+                        if ( curMenu.is( ':visible' ) ) {
+                            curMenu.slideUp(300);
+                            curElem.removeClass('open');
                         }
-                        if (event.stopPropagation) {
+                        else {
+                            curMenu.slideDown(300);
+                            curElem.addClass('open');
+                        }
+                        if ( event.stopPropagation ) {
                             event.stopPropagation();
                         } else {
                             event.cancelBubble = true;
                         }
                     }
-                });
+                }
+            });
 
-                _items.on( {
-                    click: function() {
+            $( 'body' ).on( {
 
-                        if ( $( window ).width() < 768 ) {
+                click: function( e ) {
 
-                            var curElem = $( this ),
-                                curMenu = curElem.find( 'ul' );
+                    if ( _window.width() < 768) {
 
-                            if ( curMenu.is( ':visible' ) ) {
-                                curMenu.slideUp(300);
-                                curElem.removeClass('open');
-                            }
-                            else {
-                                curMenu.slideDown(300);
-                                curElem.addClass('open');
-                            }
-                            if ( event.stopPropagation ) {
-                                event.stopPropagation();
-                            } else {
-                                event.cancelBubble = true;
-                            }
+                        var elem = $( e.target );
+
+                        var curElem = $( '.header-menu__wrap>li' ),
+                            curClose = _objBtn.parent(),
+                            curMenu = curElem.find( 'ul' );
+
+                        if ( curMenu.is( ':visible' )) {
+                            curMenu.slideUp(300);
+                            curElem.removeClass('open');
+                        }
+                        if( curClose.hasClass( 'active' ) ){
+                            curClose.removeClass('active');
                         }
                     }
-                });
+                }
+            });
+            _window.resize( function() {
 
-                $( 'body' ).on( {
+                var curElem = _objBtn.parent();
 
-                    click: function( e ) {
+                if (curElem.hasClass( 'active' )) {
+                    curElem.removeClass( 'active' );
+                }
 
-                        if ( _window.width() < 768) {
-
-                            var elem = $( e.target );
-
-                            var curElem = $( '.header-menu__wrap>li' ),
-                                curClose = _objBtn.parent(),
-                                curMenu = curElem.find( 'ul' );
-
-                            if ( curMenu.is( ':visible' )) {
-                                curMenu.slideUp(300);
-                                curElem.removeClass('open');
-                            }
-                            if( curClose.hasClass( 'active' ) ){
-                                curClose.removeClass('active');
-                            }
-                        }
-                    }
-                });
+            });
 
             _window.scroll( function() {
                 if ( $( this ).scrollTop() > 170 && _menu.hasClass( 'default' ) && _window.width() > 768 ) {
-                    _menu.fadeOut( 'fast', function() {
+                    _menu.fadeOut( 0, function() {
                         $(this).removeClass( 'default' )
                             .addClass( 'fixed transbg' )
-                            .fadeIn( 'fast' );
+                            .fadeIn(0);
                     });
                 } else if( $( this ).scrollTop() <= 170 && _menu.hasClass( 'fixed' ) ) {
-                    _menu.fadeOut( 'fast',function(){
+                    _menu.fadeOut( 0,function(){
                         $( this ).removeClass( 'fixed transbg' )
                             .addClass( 'default' )
-                            .fadeIn( 'fast' );
+                            .fadeIn(0);
                     });
                 }
             });
